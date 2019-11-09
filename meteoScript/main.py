cf75f6fb12f37650f57ad18301b944320e0ac9ab
Miquel Puigdomenech <miki2189@gmail.com>
2019-11-09 21:55:37 +0100

    added another sensor to the main method

--- meteoScript/main.py
+++ meteoScript/main.py
@@ -27,10 +27,13 @@ def add_log(tName, reading):
 
 def main():
     tName = sys.argv[1] if len(sys.argv) > 1 else "defaultTable"
+    tName2 = sys.argv[2] if len(sys.argv) > 1 else "defaultTable2"
     print ("Using table "+tName)
     while True:
-        r = bme280.getReadings()
+        r = bme280.getReadings(0x76)
         add_log(tName,r)
+        r2 = bme280.getReadings(0x77)
+        add_log(tName2,r2)
         time.sleep(300)
 
 if __name__=="__main__":
