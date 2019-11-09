b525ca9a532c55c9fa08d855b23191e938124da4
Miquel Puigdomenech <miki2189@gmail.com>
2019-11-09 21:56:12 +0100

    added method to see the diference between two temps at a specific time
    //to improve

--- meteoScript/classes.py
+++ meteoScript/classes.py
@@ -1,3 +1,5 @@
+import datetime
+
 class reading(object):
     def __init__(self, temp, press, hum, dtime):
         self.temp = temp
@@ -7,3 +9,6 @@ class reading(object):
 
     def __str__(self):
         return "At t %s: temperature = %s pressure = %s humidity = %s" % (self.dtime, self.temp, self.press, self.hum)
+    
+    def difference(self, r):
+        return reading((self.temp-r.temp),(self.press-r.press),(self.hum-r.hum), datetime.datetime.utcnow())
