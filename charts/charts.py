e877c307868c5c98f13a2f11230df5923ed176da
Miquel Puigdomenech <miki2189@gmail.com>
2019-11-12 00:16:03 +0100

    added simple method to extract some basic grafics while the webui is on construction

---
+++ charts/charts.py
@@ -0,0 +1,38 @@
+# library & dataset
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import psycopg2
+from datetime import datetime
+from dateutil import tz
+
+from_zone = tz.gettz('UTC')
+to_zone = tz.gettz('Europe/Madrid')
+
+def getInCurrentT(t):
+    utc = t.replace(tzinfo=from_zone)
+    return utc.astimezone(to_zone)
+
+def saveGrafic(tableName):
+    d = []
+    v = []
+    with psycopg2.connect('dbname=meteo') as conn:
+        with conn.cursor() as cur:
+            cur.execute('SELECT * FROM '+ tableName + ';')
+            a = cur.fetchall()
+            for i in a:
+                t = getInCurrentT(i[0])
+                d.append(str(t))
+                v.append(i[1])
+    data = {'date': d, 'temp': v}
+    df = pd.DataFrame(data, columns=['date', 'temp'])
+    df['datetime'] = pd.to_datetime(df['date'])
+    df = df.set_index('datetime')
+    df.drop(['date'], axis=1, inplace=True)
+    plt.plot('temp', data=df, marker='o', color=(0.9, 0.2, 0.5, 0.2))
+    plt.savefig(tableName+'.png')
+
+
+for t in ['difference', 'sensorbase', 'sensormodified']:
+    saveGrafic(t)
+
