334acd8ce59509e2d0be03208b91b311b0ed9752
Miquel Puigdomenech <miki2189@gmail.com>
2019-11-14 00:43:41 +0100

    added more details , fixed axis on charts

--- charts/charts.py
+++ charts/charts.py
@@ -1,10 +1,11 @@
-# library & dataset
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
-import psycopg2
 from datetime import datetime
 from dateutil import tz
+import psycopg2
+from decimal import *
+import matplotlib.dates as mdates
 
 from_zone = tz.gettz('UTC')
 to_zone = tz.gettz('Europe/Madrid')
@@ -27,17 +28,43 @@ def getPandas(tableName):
     data = {'date': d, 'temp': v}
     df = pd.DataFrame(data, columns=['date', 'temp'])
     df['datetime'] = pd.to_datetime(df['date'])
-    df = df.set_index('datetime')
+    df['temperature'] = df['temp'].astype(np.float64)
     df.drop(['date'], axis=1, inplace=True)
+    df.drop(['temp'], axis=1, inplace=True)
     return df
 
 def plotGrafic(pnd, tableName):
-    plt.rcParams["figure.figsize"] = (100,50)
-    plt.plot( 'temp' , data=pnd, marker='o', color='green', label ='baseS')
-    plt.xlabel('Time')
-    plt.ylabel('temp')
-    plt.grid(True)
+    plt.rcParams["figure.figsize"] = (150,50)
+    fig, ax = plt.subplots()
+    ax.plot('datetime','temperature', data=pnd, marker='o', color='green')
+    plt.xlabel('time')
+    plt.ylabel('temperature')
+    minT = min(min(pnd['temperature']),min(pnd['temperature']))
+    maxT = max(max(pnd['temperature']),max(pnd['temperature']))
+    ax.yaxis.set_ticks(np.arange(minT, maxT+ float(1.0), float(0.1)))
+    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=15)) 
+    plt.draw()
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+    ax.grid(True)
     plt.savefig(tableName+'.png')
+    plt.clf()
+
+def plotTwoGrafic(pndOne, pndTwo, gName):
+    plt.rcParams["figure.figsize"] = (150,50)
+    fig, ax = plt.subplots()
+    ax.plot('datetime','temperature', data=pndOne, marker='o', color='green', label ='baseS')
+    ax.plot('datetime','temperature', data=pndTwo, marker='o', color='blue', label ='modifiedS')
+    ax.legend(loc='upper left')
+    plt.xlabel('time')
+    plt.ylabel('temperature')
+    minT = min(min(pndOne['temperature']),min(pndTwo['temperature']))
+    maxT = max(max(pndOne['temperature']),max(pndTwo['temperature']))
+    ax.yaxis.set_ticks(np.arange(minT, maxT+ float(1.0), float(0.1)))
+    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=15)) 
+    plt.draw()
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+    ax.grid(True)
+    plt.savefig(gName+'.png')
     plt.clf()
 
 def saveGrafic(tableName):
@@ -45,6 +72,9 @@ def saveGrafic(tableName):
     plotGrafic(pd, tableName)        
 
 
-for t in ['difference', 'sensorbase', 'sensormodified']:
-    saveGrafic(t)
+# for t in ['difference', 'sensorbase', 'sensormodified']:
+#     saveGrafic(t)
 
+sB = getPandas('sensorbase')
+sM = getPandas('sensormodified')
+plotTwoGrafic(sB, sM, 'other')
