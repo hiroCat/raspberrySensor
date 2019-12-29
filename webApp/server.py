3aa87ad9ad6a16ccb66791d0d6babaaa3b9d4325
hiroCat <miki2189@gmail.com>
2019-12-29 23:28:24 +0100

    added socket io logic

--- webApp/server.py
+++ webApp/server.py
@@ -2,52 +2,52 @@ from flask import Flask, render_template, session, request
 from flask_session import Session
 import jinja2
 import psycopg2
+from flask_socketio import SocketIO, emit
+import json
 
 app = Flask(__name__)
 SESSION_TYPE = 'filesystem'
 app.secret_key = 'bqkMquUfb4FZ0wcQXsGlalQd49o'
 app.config.from_object(__name__)
 Session(app)
+socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/table', methods=["GET","POST"])
-def getTable():
-    return render_template('index.html')
+@socketio.on('connect', namespace='/test')
+def getConnect():
+    print ("penislol")
 
-@app.route('/chart', methods=['GET', 'POST'])
-def chart():
-    fromI = request.form.get('dateFrom', None)
-    toI = request.form.get('dateTo', None)
-    print(fromI)
-    print(toI)
-    json_string = """
-            {
-              labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','du'],
-              datasets: [{
-                  label: 'first sensor',
-                  backgroundColor: 'rgb(255, 99, 132)',
-                  borderColor: 'rgb(255, 99, 132)',
-                  data: [0, 10, 5, 2, 20, 30, 45,56,66],
-                  fill: false
+@socketio.on('charts', namespace='/test')
+def getData(message):
+    print ("dada")
+    print (message)
+    json_string = '''
+        {
+              "labels": ["January", "February", "March", "April", "May", "June", "July","du"],
+              "datasets": [{
+                  "label": "first sensor",
+                  "backgroundColor": "rgb(255, 99, 132)",
+                  "borderColor": "rgb(255, 99, 132)",
+                  "data": [0, 10, 5, 2, 20, 30, 45,56,66],
+                  "fill": false
               },
               {
-                  label: 'second sensor',
-                  backgroundColor: 'rgb(30, 211, 24)',
-                  borderColor: 'rgb(30, 211, 24)',
-                  data: [10, 1, 50, 20, 2, 3, 50,67,54],
-                  fill: false
+                  "label": "second sensor",
+                  "backgroundColor": "rgb(30, 211, 24)",
+                  "borderColor": "rgb(30, 211, 24)",
+                  "data": [10, 1, 50, 20, 2, 3, 50,67,54],
+                  "fill": false
               }]
-          } """
-    session['data'] = json_string
-    return render_template('chart.html')
+          }
+          '''.replace(" ", "")
+    socketio.emit('newData', {'data': json_string}, namespace='/test')
+
 
 if __name__ == "__main__":
     app.config['SESSION_TYPE'] = 'filesystem'
     sess = Session()
     sess.init_app(app)
-
-    app.debug = True
-    app.run()
+    socketio.run(app)
---
+++ webApp/static/js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    //connect to the socket server.
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+
+    //receive details from server
+    socket.on('newData', function(msg) {
+        var d = JSON.parse(msg.data)
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: d,
+            options: {}
+        });
+    });
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {},
+        options: {}
+    });
+
+    
+    $('#form').submit(function(){
+        socket.emit('charts', $('#form').val());
+        return false;
+    });
+
+    $("#dateRange").dateRangePicker({
+        separator: " to ",
+        getValue: function() {
+        if ($("#dateFrom").val() && $("#dateTo").val())
+            return $("#dateFrom").val() + " to " + $("#dateTo").val();
+        else return "";
+        },
+        setValue: function(s, s1, s2) {
+            $("#dateFrom").val(s1);
+            $("#dateTo").val(s2);
+        },
+        startOfWeek: "monday",
+        format: "DD/MM/YYYY"
+    });
+});
